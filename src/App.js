7b8700f36d7146aeacf9a0607243f0f9f09a3214
Ken Kotch <hello@ken.uno>
2018-01-01 20:45:51 -0700

    move firebase config to seperate file

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ import firebase from 'firebase'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import LoginForm from './components/LoginForm'
+import config from './config/config'
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: 'AIzaSyA9-4YdJBejfxzK6HHxM-gwAfT2eLL61aQ',
-      authDomain: 'manager-c2fc9.firebaseapp.com',
-      databaseURL: 'https://manager-c2fc9.firebaseio.com',
-      projectId: 'manager-c2fc9',
-      storageBucket: 'manager-c2fc9.appspot.com',
-      messagingSenderId: '624521851977'
-    }
     firebase.initializeApp(config)
   }
 
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyA9-4YdJBejfxzK6HHxM-gwAfT2eLL61aQ',
+  authDomain: 'manager-c2fc9.firebaseapp.com',
+  databaseURL: 'https://manager-c2fc9.firebaseio.com',
+  projectId: 'manager-c2fc9',
+  storageBucket: 'manager-c2fc9.appspot.com',
+  messagingSenderId: '624521851977'
+}
