c6c6262a6cbcebe154307ecfa930123bd474b9c7
Ken Kotch <hello@ken.uno>
2018-01-02 11:11:07 -0700

    allow edit employees

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import { Text, View, TouchableWithoutFeedback } from 'react-native'
 import { CardSection } from './common'
 
 class ListItem extends Component {
-  componentWillMount() {
-    console.log('ListItem mounted')
+  onRowPress() {
+    Actions.employeeEdit({ employee: this.props.employee })
   }
 
   render() {
     const { name } = this.props.employee
 
     return (
-      <CardSection>
-        <Text style={ styles.titleStyle }>
-          { name }
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={ this.onRowPress.bind(this) }>
+        <View>
+          <CardSection>
+            <Text style={ styles.titleStyle }>
+              { name }
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
